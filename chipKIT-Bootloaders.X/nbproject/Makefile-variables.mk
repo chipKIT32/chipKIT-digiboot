bb0a3cd84439a496d13b543f953a6a48f6facc0e
Brian Schmalz <brian@schmalzhaus.com>
2016-10-10 17:08:40 -0500

    Initial checkin from Digilent source as of Oct 10, 2016.

---
+++ chipKIT-Bootloaders.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,244 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# MX1_EXAMPLE_UART configuration
+CND_ARTIFACT_DIR_MX1_EXAMPLE_UART=dist/MX1_EXAMPLE_UART/production
+CND_ARTIFACT_NAME_MX1_EXAMPLE_UART=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX1_EXAMPLE_UART=dist/MX1_EXAMPLE_UART/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX1_EXAMPLE_UART=${CND_DISTDIR}/MX1_EXAMPLE_UART/package
+CND_PACKAGE_NAME_MX1_EXAMPLE_UART=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX1_EXAMPLE_UART=${CND_DISTDIR}/MX1_EXAMPLE_UART/package/chipkit-bootloaders.x.tar
+# MX1_Cmod configuration
+CND_ARTIFACT_DIR_MX1_Cmod=dist/MX1_Cmod/production
+CND_ARTIFACT_NAME_MX1_Cmod=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX1_Cmod=dist/MX1_Cmod/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX1_Cmod=${CND_DISTDIR}/MX1_Cmod/package
+CND_PACKAGE_NAME_MX1_Cmod=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX1_Cmod=${CND_DISTDIR}/MX1_Cmod/package/chipkit-bootloaders.x.tar
+# DP32 configuration
+CND_ARTIFACT_DIR_DP32=dist/DP32/production
+CND_ARTIFACT_NAME_DP32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_DP32=dist/DP32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_DP32=${CND_DISTDIR}/DP32/package
+CND_PACKAGE_NAME_DP32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_DP32=${CND_DISTDIR}/DP32/package/chipkit-bootloaders.x.tar
+# MX2_MINI_USB configuration
+CND_ARTIFACT_DIR_MX2_MINI_USB=dist/MX2_MINI_USB/production
+CND_ARTIFACT_NAME_MX2_MINI_USB=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX2_MINI_USB=dist/MX2_MINI_USB/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX2_MINI_USB=${CND_DISTDIR}/MX2_MINI_USB/package
+CND_PACKAGE_NAME_MX2_MINI_USB=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX2_MINI_USB=${CND_DISTDIR}/MX2_MINI_USB/package/chipkit-bootloaders.x.tar
+# Uno32 configuration
+CND_ARTIFACT_DIR_Uno32=dist/Uno32/production
+CND_ARTIFACT_NAME_Uno32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_Uno32=dist/Uno32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_Uno32=${CND_DISTDIR}/Uno32/package
+CND_PACKAGE_NAME_Uno32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_Uno32=${CND_DISTDIR}/Uno32/package/chipkit-bootloaders.x.tar
+# MX3cK_128 configuration
+CND_ARTIFACT_DIR_MX3cK_128=dist/MX3cK_128/production
+CND_ARTIFACT_NAME_MX3cK_128=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX3cK_128=dist/MX3cK_128/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX3cK_128=${CND_DISTDIR}/MX3cK_128/package
+CND_PACKAGE_NAME_MX3cK_128=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX3cK_128=${CND_DISTDIR}/MX3cK_128/package/chipkit-bootloaders.x.tar
+# MX3cK_512 configuration
+CND_ARTIFACT_DIR_MX3cK_512=dist/MX3cK_512/production
+CND_ARTIFACT_NAME_MX3cK_512=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX3cK_512=dist/MX3cK_512/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX3cK_512=${CND_DISTDIR}/MX3cK_512/package
+CND_PACKAGE_NAME_MX3cK_512=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX3cK_512=${CND_DISTDIR}/MX3cK_512/package/chipkit-bootloaders.x.tar
+# uC32 configuration
+CND_ARTIFACT_DIR_uC32=dist/uC32/production
+CND_ARTIFACT_NAME_uC32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_uC32=dist/uC32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_uC32=${CND_DISTDIR}/uC32/package
+CND_PACKAGE_NAME_uC32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_uC32=${CND_DISTDIR}/uC32/package/chipkit-bootloaders.x.tar
+# MX4cK configuration
+CND_ARTIFACT_DIR_MX4cK=dist/MX4cK/production
+CND_ARTIFACT_NAME_MX4cK=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX4cK=dist/MX4cK/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX4cK=${CND_DISTDIR}/MX4cK/package
+CND_PACKAGE_NAME_MX4cK=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX4cK=${CND_DISTDIR}/MX4cK/package/chipkit-bootloaders.x.tar
+# MX4_UART configuration
+CND_ARTIFACT_DIR_MX4_UART=dist/MX4_UART/production
+CND_ARTIFACT_NAME_MX4_UART=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX4_UART=dist/MX4_UART/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX4_UART=${CND_DISTDIR}/MX4_UART/package
+CND_PACKAGE_NAME_MX4_UART=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX4_UART=${CND_DISTDIR}/MX4_UART/package/chipkit-bootloaders.x.tar
+# MX4_USB configuration
+CND_ARTIFACT_DIR_MX4_USB=dist/MX4_USB/production
+CND_ARTIFACT_NAME_MX4_USB=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX4_USB=dist/MX4_USB/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX4_USB=${CND_DISTDIR}/MX4_USB/package
+CND_PACKAGE_NAME_MX4_USB=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX4_USB=${CND_DISTDIR}/MX4_USB/package/chipkit-bootloaders.x.tar
+# MX4_SD_USB configuration
+CND_ARTIFACT_DIR_MX4_SD_USB=dist/MX4_SD_USB/production
+CND_ARTIFACT_NAME_MX4_SD_USB=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX4_SD_USB=dist/MX4_SD_USB/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX4_SD_USB=${CND_DISTDIR}/MX4_SD_USB/package
+CND_PACKAGE_NAME_MX4_SD_USB=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX4_SD_USB=${CND_DISTDIR}/MX4_SD_USB/package/chipkit-bootloaders.x.tar
+# UBW32_MX460 configuration
+CND_ARTIFACT_DIR_UBW32_MX460=dist/UBW32_MX460/production
+CND_ARTIFACT_NAME_UBW32_MX460=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_UBW32_MX460=dist/UBW32_MX460/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_UBW32_MX460=${CND_DISTDIR}/UBW32_MX460/package
+CND_PACKAGE_NAME_UBW32_MX460=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_UBW32_MX460=${CND_DISTDIR}/UBW32_MX460/package/chipkit-bootloaders.x.tar
+# WF32 configuration
+CND_ARTIFACT_DIR_WF32=dist/WF32/production
+CND_ARTIFACT_NAME_WF32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_WF32=dist/WF32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_WF32=${CND_DISTDIR}/WF32/package
+CND_PACKAGE_NAME_WF32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_WF32=${CND_DISTDIR}/WF32/package/chipkit-bootloaders.x.tar
+# FG32 configuration
+CND_ARTIFACT_DIR_FG32=dist/FG32/production
+CND_ARTIFACT_NAME_FG32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_FG32=dist/FG32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_FG32=${CND_DISTDIR}/FG32/package
+CND_PACKAGE_NAME_FG32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_FG32=${CND_DISTDIR}/FG32/package/chipkit-bootloaders.x.tar
+# MAX32 configuration
+CND_ARTIFACT_DIR_MAX32=dist/MAX32/production
+CND_ARTIFACT_NAME_MAX32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MAX32=dist/MAX32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MAX32=${CND_DISTDIR}/MAX32/package
+CND_PACKAGE_NAME_MAX32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MAX32=${CND_DISTDIR}/MAX32/package/chipkit-bootloaders.x.tar
+# MX7cK configuration
+CND_ARTIFACT_DIR_MX7cK=dist/MX7cK/production
+CND_ARTIFACT_NAME_MX7cK=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX7cK=dist/MX7cK/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX7cK=${CND_DISTDIR}/MX7cK/package
+CND_PACKAGE_NAME_MX7cK=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX7cK=${CND_DISTDIR}/MX7cK/package/chipkit-bootloaders.x.tar
+# MX7_UART configuration
+CND_ARTIFACT_DIR_MX7_UART=dist/MX7_UART/production
+CND_ARTIFACT_NAME_MX7_UART=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX7_UART=dist/MX7_UART/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX7_UART=${CND_DISTDIR}/MX7_UART/package
+CND_PACKAGE_NAME_MX7_UART=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX7_UART=${CND_DISTDIR}/MX7_UART/package/chipkit-bootloaders.x.tar
+# MX7_USB configuration
+CND_ARTIFACT_DIR_MX7_USB=dist/MX7_USB/production
+CND_ARTIFACT_NAME_MX7_USB=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX7_USB=dist/MX7_USB/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX7_USB=${CND_DISTDIR}/MX7_USB/package
+CND_PACKAGE_NAME_MX7_USB=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX7_USB=${CND_DISTDIR}/MX7_USB/package/chipkit-bootloaders.x.tar
+# MX7cK_USB_SPLIT_FLASH configuration
+CND_ARTIFACT_DIR_MX7cK_USB_SPLIT_FLASH=dist/MX7cK_USB_SPLIT_FLASH/production
+CND_ARTIFACT_NAME_MX7cK_USB_SPLIT_FLASH=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MX7cK_USB_SPLIT_FLASH=dist/MX7cK_USB_SPLIT_FLASH/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MX7cK_USB_SPLIT_FLASH=${CND_DISTDIR}/MX7cK_USB_SPLIT_FLASH/package
+CND_PACKAGE_NAME_MX7cK_USB_SPLIT_FLASH=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MX7cK_USB_SPLIT_FLASH=${CND_DISTDIR}/MX7cK_USB_SPLIT_FLASH/package/chipkit-bootloaders.x.tar
+# UBW32_MX795 configuration
+CND_ARTIFACT_DIR_UBW32_MX795=dist/UBW32_MX795/production
+CND_ARTIFACT_NAME_UBW32_MX795=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_UBW32_MX795=dist/UBW32_MX795/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_UBW32_MX795=${CND_DISTDIR}/UBW32_MX795/package
+CND_PACKAGE_NAME_UBW32_MX795=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_UBW32_MX795=${CND_DISTDIR}/UBW32_MX795/package/chipkit-bootloaders.x.tar
+# UDB32_PIM configuration
+CND_ARTIFACT_DIR_UDB32_PIM=dist/UDB32_PIM/production
+CND_ARTIFACT_NAME_UDB32_PIM=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_UDB32_PIM=dist/UDB32_PIM/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_UDB32_PIM=${CND_DISTDIR}/UDB32_PIM/package
+CND_PACKAGE_NAME_UDB32_PIM=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_UDB32_PIM=${CND_DISTDIR}/UDB32_PIM/package/chipkit-bootloaders.x.tar
+# UDB32_MX2_DIP configuration
+CND_ARTIFACT_DIR_UDB32_MX2_DIP=dist/UDB32_MX2_DIP/production
+CND_ARTIFACT_NAME_UDB32_MX2_DIP=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_UDB32_MX2_DIP=dist/UDB32_MX2_DIP/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_UDB32_MX2_DIP=${CND_DISTDIR}/UDB32_MX2_DIP/package
+CND_PACKAGE_NAME_UDB32_MX2_DIP=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_UDB32_MX2_DIP=${CND_DISTDIR}/UDB32_MX2_DIP/package/chipkit-bootloaders.x.tar
+# WF32z configuration
+CND_ARTIFACT_DIR_WF32z=dist/WF32z/production
+CND_ARTIFACT_NAME_WF32z=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_WF32z=dist/WF32z/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_WF32z=${CND_DISTDIR}/WF32z/package
+CND_PACKAGE_NAME_WF32z=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_WF32z=${CND_DISTDIR}/WF32z/package/chipkit-bootloaders.x.tar
+# WiFire configuration
+CND_ARTIFACT_DIR_WiFire=dist/WiFire/production
+CND_ARTIFACT_NAME_WiFire=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_WiFire=dist/WiFire/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_WiFire=${CND_DISTDIR}/WiFire/package
+CND_PACKAGE_NAME_WiFire=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_WiFire=${CND_DISTDIR}/WiFire/package/chipkit-bootloaders.x.tar
+# zUno configuration
+CND_ARTIFACT_DIR_zUno=dist/zUno/production
+CND_ARTIFACT_NAME_zUno=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_zUno=dist/zUno/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_zUno=${CND_DISTDIR}/zUno/package
+CND_PACKAGE_NAME_zUno=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_zUno=${CND_DISTDIR}/zUno/package/chipkit-bootloaders.x.tar
+# Pi configuration
+CND_ARTIFACT_DIR_Pi=dist/Pi/production
+CND_ARTIFACT_NAME_Pi=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_Pi=dist/Pi/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_Pi=${CND_DISTDIR}/Pi/package
+CND_PACKAGE_NAME_Pi=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_Pi=${CND_DISTDIR}/Pi/package/chipkit-bootloaders.x.tar
+# MfgWiFire configuration
+CND_ARTIFACT_DIR_MfgWiFire=dist/MfgWiFire/production
+CND_ARTIFACT_NAME_MfgWiFire=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MfgWiFire=dist/MfgWiFire/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MfgWiFire=${CND_DISTDIR}/MfgWiFire/package
+CND_PACKAGE_NAME_MfgWiFire=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MfgWiFire=${CND_DISTDIR}/MfgWiFire/package/chipkit-bootloaders.x.tar
+# MfgWF32 configuration
+CND_ARTIFACT_DIR_MfgWF32=dist/MfgWF32/production
+CND_ARTIFACT_NAME_MfgWF32=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_MfgWF32=dist/MfgWF32/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_MfgWF32=${CND_DISTDIR}/MfgWF32/package
+CND_PACKAGE_NAME_MfgWF32=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_MfgWF32=${CND_DISTDIR}/MfgWF32/package/chipkit-bootloaders.x.tar
+# LogicFire configuration
+CND_ARTIFACT_DIR_LogicFire=dist/LogicFire/production
+CND_ARTIFACT_NAME_LogicFire=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_LogicFire=dist/LogicFire/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_LogicFire=${CND_DISTDIR}/LogicFire/package
+CND_PACKAGE_NAME_LogicFire=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_LogicFire=${CND_DISTDIR}/LogicFire/package/chipkit-bootloaders.x.tar
+# WiFIRE_80MHz configuration
+CND_ARTIFACT_DIR_WiFIRE_80MHz=dist/WiFIRE_80MHz/production
+CND_ARTIFACT_NAME_WiFIRE_80MHz=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_WiFIRE_80MHz=dist/WiFIRE_80MHz/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_WiFIRE_80MHz=${CND_DISTDIR}/WiFIRE_80MHz/package
+CND_PACKAGE_NAME_WiFIRE_80MHz=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_WiFIRE_80MHz=${CND_DISTDIR}/WiFIRE_80MHz/package/chipkit-bootloaders.x.tar
+# WiFIRE_EF configuration
+CND_ARTIFACT_DIR_WiFIRE_EF=dist/WiFIRE_EF/production
+CND_ARTIFACT_NAME_WiFIRE_EF=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_WiFIRE_EF=dist/WiFIRE_EF/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_WiFIRE_EF=${CND_DISTDIR}/WiFIRE_EF/package
+CND_PACKAGE_NAME_WiFIRE_EF=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_WiFIRE_EF=${CND_DISTDIR}/WiFIRE_EF/package/chipkit-bootloaders.x.tar
+# OpenScope configuration
+CND_ARTIFACT_DIR_OpenScope=dist/OpenScope/production
+CND_ARTIFACT_NAME_OpenScope=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_OpenScope=dist/OpenScope/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_OpenScope=${CND_DISTDIR}/OpenScope/package
+CND_PACKAGE_NAME_OpenScope=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_OpenScope=${CND_DISTDIR}/OpenScope/package/chipkit-bootloaders.x.tar
+# uC32_350 configuration
+CND_ARTIFACT_DIR_uC32_350=dist/uC32_350/production
+CND_ARTIFACT_NAME_uC32_350=chipKIT-Bootloaders.X.production.hex
+CND_ARTIFACT_PATH_uC32_350=dist/uC32_350/production/chipKIT-Bootloaders.X.production.hex
+CND_PACKAGE_DIR_uC32_350=${CND_DISTDIR}/uC32_350/package
+CND_PACKAGE_NAME_uC32_350=chipkit-bootloaders.x.tar
+CND_PACKAGE_PATH_uC32_350=${CND_DISTDIR}/uC32_350/package/chipkit-bootloaders.x.tar
